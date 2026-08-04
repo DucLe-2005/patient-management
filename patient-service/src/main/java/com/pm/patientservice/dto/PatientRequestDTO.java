e5fa403b3bb576322d46c7ec91b7ed20e0210661
AnhDuc <ducleanh2005@gmail.com>
2026-08-04 03:55:59 -0500

    feat: handle exceptions with centralized exception handler

--- patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -6,51 +6,12 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class PatientRequestDTO {
-    public @NotBlank(message = "Name is required") @Size(max = 100, message = "Name cannot exceed 100 characters") String getName() {
-        return name;
-    }
-
-    public void setName(@NotBlank(message = "Name is required") @Size(max = 100, message = "Name cannot exceed 100 characters") String name) {
-        this.name = name;
-    }
-
-    public @NotBlank @Email(message = "Email should be valid") String getEmail() {
-        return email;
-    }
-
-    public void setEmail(@NotBlank @Email(message = "Email should be valid") String email) {
-        this.email = email;
-    }
-
-    public @NotBlank(message = "Address is required") String getAddress() {
-        return address;
-    }
-
-    public void setAddress(@NotBlank(message = "Address is required") String address) {
-        this.address = address;
-    }
-
-    public @NotBlank(message = "Date of birth is required") String getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(@NotBlank(message = "Date of birth is required") String dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public @NotNull(message = "Registered date is required") String getRegisteredDate() {
-        return registeredDate;
-    }
-
-    public void setRegisteredDate(@NotNull(message = "Registered date is required") String registeredDate) {
-        this.registeredDate = registeredDate;
-    }
 
     @NotBlank(message = "Name is required")
     @Size(max = 100, message = "Name cannot exceed 100 characters")
     private String name;
 
-    @NotBlank
+    @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
     private String email;
 
@@ -63,5 +24,43 @@ public class PatientRequestDTO {
     @NotNull(message = "Registered date is required")
     private String registeredDate;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
 }
